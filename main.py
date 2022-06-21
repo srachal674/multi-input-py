72887362e65872530fedaf30b3ea5e51c858ad29
Shannon Rachal <srachal674@gmail.com>
2022-06-21 13:38:11 -0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-
-input.button_is_pressed(Button.A)
-def greeting():
+if(input.button_is_pressed(Button.A))
     basic.show_string("Shannon")
 
-input.button_is_pressed(Button.B)
-def num():
+if (input.button_is_pressed(Button.B))
     basic.show_number(randint(1, 100))
 
